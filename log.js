a288f843d849cdf8e5105099bed8fb3df640acad
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2023-05-30 20:28:54 +0200

    refactor: random moves

--- log.js
+++ log.js
@@ -1,6 +1,7 @@
 import { table } from 'table'
 import { state } from './state.js'
 
+// logs game with training statistics
 export function log () {
   const owners = state.map((row) => row.map((piece) => piece?.owner))
   console.clear()
@@ -30,5 +31,16 @@ export function log () {
         colSpan: 2
       }
     ]
+  }))
+}
+
+// logs only game
+export function logGame () {
+  const owners = state.map((row) => row.map((piece) => piece?.owner))
+  console.clear()
+  console.log(table(owners.map((row) => row.map((piece) => piece === 0 ? '\u2592\u2592' : piece === 1 ? '\u2593\u2593' : '')), {
+    columnDefault: {
+      width: 15
+    }
   }))
 }
---
+++ random.js
@@ -0,0 +1,26 @@
+import { move, result, reset } from './state.js'
+import { logGame } from './log.js'
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+async function randomMove (owner) {
+  while (true) if (move(Math.round(Math.random() * 2), owner, Math.round(Math.random() * 2)).done) break
+  logGame()
+  await delay(1500)
+
+  if (result().done) {
+    console.log(result())
+    process.exit(1)
+  }
+}
+
+while (!result().done) {
+  reset()
+  logGame()
+  await delay(1500)
+
+  for (let i = 0; i < 10; i += 1) {
+    await randomMove(0)
+    await randomMove(1)
+  }
+}
