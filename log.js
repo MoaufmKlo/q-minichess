705f4ab6fa76922500b0c2a62418faab81b83b79
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2023-06-01 09:11:29 +0200

    feat: in-depth logging

--- log.js
+++ log.js
@@ -1,23 +1,37 @@
 import { table } from 'table'
 import { state } from './state.js'
+import humanizeDuration from 'humanize-duration'
 
 // logs game with training statistics
-export function log () {
+export function log ({ episode, episodes, states, epochs, rewards, ms }) {
   const owners = state.map((row) => row.map((piece) => piece?.owner))
   console.clear()
   console.log(table([
     ...owners.map((row) => row.map((piece) => piece === 0 ? '\u2592\u2592' : piece === 1 ? '\u2593\u2593' : '')),
     [
-      'Avg. timestamps per episode',,
-      ''
+      'Episodes',,
+      `${episode}/${episodes} (${states} states)`
     ],
     [
-      'Avg. penalties per episode',,
-      ''
+      'Avg. epochs per episode',,
+      `${epochs.reduce((sum, a) => sum + a) / epochs.length}`.substring(0, 15)
+    ],
+    [
+      'Avg. rewards per episode',,
+      `${rewards.reduce((sum, a) => sum + a) / rewards.length}`.substring(0, 15)
+    ],
+    [
+      'Time left',,
+      `${humanizeDuration(ms / episode * episodes - ms, { maxDecimalPoints: 0 })} (total ${humanizeDuration(ms / episode * episodes, { maxDecimalPoints: 0 })})`
     ]
   ], {
     columnDefault: {
-      width: 15
+      width: 20
+    },
+    columns: {
+      2: {
+        width: 40
+      }
     },
     spanningCells: [
       {
@@ -28,6 +42,16 @@ export function log () {
       {
         col: 0,
         row: 4,
+        colSpan: 2
+      },
+      {
+        col: 0,
+        row: 5,
+        colSpan: 2
+      },
+      {
+        col: 0,
+        row: 6,
         colSpan: 2
       }
     ]
